9afa72f1fcb0c1a09a9956dce27dd9efd06838dc
Johnny Silva <johnnydev.js@gmail.com>
2025-06-25 17:32:21 -0300

    add .editorconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended'
+import pluginReact from 'eslint-plugin-react'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import globals from 'globals'
+import { dirname } from 'path'
+import tseslint from 'typescript-eslint'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  eslintPluginPrettier,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'import/no-anonymous-default-export': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  semi: false,
+  endOfLine: 'auto',
+
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
